cd09caefcf980bd3e7432b294d5e96b1b415b24e
Fauntleroy <the.kaempfer@gmail.com>
2013-02-21 15:19:31 -0800

    refactor page processing methods
    give indices precedence

--- solidus.js
+++ solidus.js
@@ -9,47 +9,89 @@ var async = require('async');
 var express = require('express');
 var expose = require('express-expose');
 var router = express();
-var walk = require('walk');
+var chokidar = require('chokidar');
 var request = require('request');
 var igneous = require('igneous');
 
 var views_path = path.join( __dirname, SITE_DIR, 'views' );
 var preprocessors_path = path.join( __dirname, SITE_DIR, 'preprocessors' );
 
-var walker = walk.walk( views_path, {
-	followLinks: false
+var watcher = chokidar.watch( views_path, {
+	persistent: true
 });
 
-// Walk through files and create routes
-walker.on( 'file', function( root, stat, next ){
+var pages = {};
 
-	var absolute_path = path.join( root, stat.name );
-	var relative_path = path.relative( views_path, absolute_path );
-	var path_bits = relative_path.split('.');
-	var route = '/'+ path_bits[0];
-	route = route.replace( '/index', '' ); // replace indexes with base routes
-	route = route.replace( /{([a-z_-]*)}/ig, ':$1' ); // replace dynamic bits
-	if( route === '' ) route = '/';
-	var context = {
-		parameters: {},
-		resources: {}
+var Page = function( page_path, options ){
+
+	var page = this;
+
+	this.path = page_path;
+	this.relative_path = path.relative( views_path, page_path );
+	this.options = options || {};
+
+	this.createRoute = function(){
+
+		page.is_index = /index\.hbs$/i.test( this.relative_path );
+		var route = this.relative_path.replace( /\.[a-z0-9]+$/i, '' );
+		var route = '/'+ route;
+		route = route.replace( '/index', '' ); // replace indexes with base routes
+		route = route.replace( /{([a-z_-]*)}/ig, ':$1' ); // replace dynamic bits
+		if( route === '' ) route = '/';
+		page.route = route;
+
+		// Ensure we don't overwrite index routes
+		var existing_index;
+
+		for( var page_path in pages ){
+			var current_page = pages[page_path];
+			var routes_match = page.route === current_page.route;
+			if( routes_match && current_page.is_index ) return existing_index = true;
+		}
+
+		if( !existing_index ){
+
+			router.routes.get = _( router.routes.get ).reject( function( current_route ){
+				console.log( route === current_route.path );
+				return current_route.path === route;
+			});
+
+			router.get( route, function( req, res ){
+				page.render( req, res );
+			});
+
+			router.get( route +'.json', function( req, res ){
+				page.render( req, res, {
+					json: true
+				});
+			});
+
+		}
+		else {
+			page.route = null;
+		}
+console.log( _( router.routes.get ).pluck('path') );
+		return route;
+
 	};
 
-	var extractViewParameters = function( callback ){
+	this.parsePage = function( callback ){
 
-		fs.readFile( absolute_path, DEFAULT_ENCODING, function( err, data ){
+		fs.readFile( this.path, DEFAULT_ENCODING, function( err, data ){
 
 			var params = {};
 			var params_exec = /^{{!\s([\S\s]+?)\s}}/.exec( data );
 			params = ( params_exec )? JSON.parse( params_exec[1] ): {};
 
-			if( callback ) callback( params );
+			page.params = params;
 			
+			if( callback ) callback( params );
+
 		});
 
 	};
 
-	var fetchResources = function( resources, callback ){
+	this.fetchResources = function( resources, callback ){
 
 		var resources_data = {};
 
@@ -72,7 +114,7 @@ walker.on( 'file', function( root, stat, next ){
 
 	};
 
-	var preprocessContext = function( context, preprocessor ){
+	this.preprocessContext = function( context, preprocessor ){
 
 		if( preprocessor ){
 			var preprocessor_path = path.join( preprocessors_path, preprocessor );
@@ -85,9 +127,15 @@ walker.on( 'file', function( root, stat, next ){
 
 	};
 
-	var renderView = function( req, res, options ){
+	this.render = function( req, res, options ){
 
 		options = options || {};
+
+		var context = {
+			parameters: {},
+			query: {},
+			resources: {}
+		};
 
 		// req.params is actually an array with crap stuck to it
 		// so we have to parse that stuff out into a real object
@@ -95,38 +143,41 @@ walker.on( 'file', function( root, stat, next ){
 		for( var key in req.params ) parameters[key] = req.params[key];
 		context.parameters = parameters;
 
-		extractViewParameters( function( parameters ){
-
-			view_params = parameters;
-
-			fetchResources( view_params.resources, function( resources ){
-				context.resources = resources;
-				context = preprocessContext( context, view_params.preprocessor );
-				if( options.json ) return res.json( context );
-				res.expose( context, 'solidus.context' );
-				res.render( relative_path, context );
-			});
-			
+		this.fetchResources( page.params.resources, function( resources ){
+			context.resources = resources;
+			context = page.preprocessContext( context, page.params.preprocessor );
+			if( options.json ) return res.json( context );
+			res.expose( context, 'solidus.context' );
+			res.render( page.path, context );
 		});
 
 	};
 
-	// serve up the normal page
-	router.get( route, function( req, res ){
+	this.destroy = function(){
 
-		renderView( req, res );
+		// find the route
+		// delete it
 
-	});
+	};
 
-	router.get( route +'.json', function( req, res ){
+	this.createRoute();
+	this.parsePage();
 
-		renderView( req, res, {
-			json: true
-		});
+};
 
-	});
+watcher.on( 'add', function( path ){
 
-	next();
+	var page = new Page( path );
+	pages[path] = page;
+
+});
+
+watcher.on( 'remove', function( path ){
+
+	// remove this page
+
+	pages[path].destroy();
+	delete pages[path];
 
 });
 
@@ -162,6 +213,6 @@ router.set( 'views', views_path );
 var assets_path = path.join( __dirname, SITE_DIR, 'assets' );
 var api_mock_path = path.join( __dirname, API_MOCK_DIR );
 
-router.use( express.static( assets_path ) );
+//router.use( express.static( assets_path ) );
 router.use( '/api', express.static( api_mock_path ) );
 router.listen( 8080 );
